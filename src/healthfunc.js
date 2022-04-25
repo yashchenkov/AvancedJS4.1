68b0c0eeff88b7812b7d1e7e8b2261f549c44ad8
Yashchenko Victor <Andrey@Komp11.local>
2022-04-25 23:43:42 +0300

    solving errors 4

--- src/healthfunc.js
+++ src/healthfunc.js
@@ -5,8 +5,5 @@ export default function healthFunc(obj) {
   if (obj.health < 50 && obj.health >= 15) {
     return 'wounded';
   }
-  if (obj.health < 15) {
-    return 'critical';
-  }
-  return '';
+  return 'critical';
 }
