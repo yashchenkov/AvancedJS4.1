5261defc4624d4b9b915a5cb19588ed56f642d6b
Yashchenko Victor <Andrey@Komp11.local>
2022-04-25 23:11:17 +0300

    simple change

--- src/__tests__/healthTest.js
+++ src/__tests__/healthTest.js
@@ -1,31 +1,31 @@
-import healthFunc from '../healthfunc.js';
+import healthFunc from '../healthfunc';
 
 test('expected healthy value test', () => {
-	const character = {
-		name: 'Маг',
-		health: 90
-	};
-	const expected = 'healthy';
-	const received = healthFunc(character);
-	expect(received).toBe(expected);
+  const character = {
+    name: 'Маг',
+    health: 90
+};
+  const expected = 'healthy';
+  const received = healthFunc(character);
+  expect(received).toBe(expected);
 });
 
 test('expected wounded value test', () => {
-	const character = {
-		name: 'Маг',
-		health: 45
-	};
-	const expected = 'wounded';
-	const received = healthFunc(character);
-	expect(received).toBe(expected);
+  const character = {
+    name: 'Маг',
+    health: 45
+};
+  const expected = 'wounded';
+  const received = healthFunc(character);
+  expect(received).toBe(expected);
 });
 
 test('expected critical value test', () => {
-	const character = {
-		name: 'Маг',
-		health: 10
-	};
-	const expected = 'critical';
-	const received = healthFunc(character);
-	expect(received).toBe(expected);
+  const character = {
+    name: 'Маг',
+    health: 10
+  };
+  const expected = 'critical';
+  const received = healthFunc(character);
+  expect(received).toBe(expected);
 });
